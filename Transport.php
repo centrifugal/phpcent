759e117bce69d5a7a8a6860d9bba27a2f7f7e448
dsoldatenko <d.soldatenko@co.wapstart.ru>
2015-07-07 01:00:09 +0300

    unsafe ssl

--- Transport.php
+++ Transport.php
@@ -10,6 +10,18 @@ namespace phpcent;
 
 class Transport implements ITransport
 {
+    const SAFE = 1;
+    const UNSAFE = 2;
+
+    protected static $safety = self::SAFE;
+
+    /**
+     * @param mixed $safety
+     */
+    public static function setSafety($safety)
+    {
+        self::$safety = $safety;
+    }
 
     public function communicate($host, $projectKey, $data)
     {
@@ -17,13 +29,27 @@ class Transport implements ITransport
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data, '', '&'));
+
+        if (self::$safety === Transport::UNSAFE) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        }
+
+        $postData = http_build_query($data, '', '&');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+
         $response = curl_exec($ch);
         $headers = curl_getinfo($ch);
         curl_close($ch);
+
         if (empty($headers["http_code"]) || ($headers["http_code"] != 200)) {
-            throw new \Exception("Response code: " . $headers["http_code"] . PHP_EOL . "Body: " . $response);
+            throw new \Exception("Response code: "
+                                 . $headers["http_code"]
+                                 . PHP_EOL
+                                 . "Body: "
+                                 . $response
+            );
         }
+
         $answer = json_decode($response, true);
 
         return $answer;

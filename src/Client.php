737928eaaf9b16010e8fb3602f1081e852acef67
Alexander Emelin <frvzmb@gmail.com>
2021-09-06 15:23:19 +0300

    rm rpc, fix channels

--- src/Client.php
+++ src/Client.php
@@ -1,4 +1,6 @@
-<?php namespace phpcent;
+<?php
+
+namespace phpcent;
 
 /**
  * Centrifugo API Client
@@ -270,28 +272,16 @@ class Client
     }
 
     /**
-     * Send rpc.
-     *
-     * @return mixed
-     */
-    public function rpc($method, $params)
-    {
-        return $this->send('rpc', array(
-            'method' => $method,
-            'params' => $params,
-        ));
-    }
-
-    /**
      * Get all active channels.
      * 
-     * @param string $client (optional)
      * @param string $pattern (optional)
      * @return mixed
      */
     public function channels($pattern = '')
     {
-        return $this->rpc("getChannels", array('pattern' => $pattern));
+        return $this->send("channels", array(
+            'pattern' => $pattern,
+        ));
     }
 
     /**
@@ -366,24 +356,25 @@ class Client
     /*
     * Function added for backward compatibility with PHP version < 5.5
     */
-    public function _json_last_error_msg() {
-      if (function_exists('json_last_error_msg')) {
-        return json_last_error_msg();
-      }
-      static $ERRORS = array(
-        JSON_ERROR_NONE => 'No error',
-        JSON_ERROR_DEPTH => 'Maximum stack depth exceeded',
-        JSON_ERROR_STATE_MISMATCH => 'State mismatch (invalid or malformed JSON)',
-        JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
-        JSON_ERROR_SYNTAX => 'Syntax error',
-        JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
-      );
+    public function _json_last_error_msg()
+    {
+        if (function_exists('json_last_error_msg')) {
+            return json_last_error_msg();
+        }
+        static $ERRORS = array(
+            JSON_ERROR_NONE => 'No error',
+            JSON_ERROR_DEPTH => 'Maximum stack depth exceeded',
+            JSON_ERROR_STATE_MISMATCH => 'State mismatch (invalid or malformed JSON)',
+            JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
+            JSON_ERROR_SYNTAX => 'Syntax error',
+            JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded'
+        );
 
-      $error = json_last_error();
-      return isset($ERRORS[$error]) ? $ERRORS[$error] : 'Unknown error';
-  }
+        $error = json_last_error();
+        return isset($ERRORS[$error]) ? $ERRORS[$error] : 'Unknown error';
+    }
 
-  private function send($method, $params = array())
+    private function send($method, $params = array())
     {
         $response = \json_decode($this->request($method, $params), $this->useAssoc);
         if (JSON_ERROR_NONE !== json_last_error()) {
@@ -443,9 +434,9 @@ class Client
         if (empty($headers["http_code"]) || ($headers["http_code"] != 200)) {
             throw new \Exception(
                 "Response code: "
-                . $headers["http_code"]
-                . PHP_EOL
-                . "cURL error: " . $error . PHP_EOL
+                    . $headers["http_code"]
+                    . PHP_EOL
+                    . "cURL error: " . $error . PHP_EOL
             );
         }
         return $data;
